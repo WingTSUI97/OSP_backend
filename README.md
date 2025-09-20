5059fbe8a261fd0622d89f2f91edcb51624efafc
WingTSUI97 <tkwing1997@gmail.com>
2025-09-20 15:20:03 +0800

    install & connect MongoDB through ODM-mongoose, modify README

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-" OSP_backend" 
+# Online Survey Platform(Backend)
+
+Build a small RESTful API server for creating surveys, retrieving them by a public token, submitting responses, and listing responses.
+
+## Features
+- Express
+- REST API
+- MonggoDB
+- Mongoose
+
+## Requirements
+- [node & npm](https://nodejs.org/en)
+- [git](https://docs.github.com/en/get-started/git-basics/set-up-git)
+
+## Installation
+- `git clone https://github.com/WingTSUI97/OSP_backend`
+
+### GET Routes
+- visit http://localhost:3000
--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 import express from 'express'
+import mongoose from 'mongoose';
 
 const app = express()
 
-/*app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000')
-}) 
-  */
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
 
 app.get('/', (req, res) => {
-    res.send('Hello, from Node API Server, Updated');
+    res.send('Hello, from OSP_backend Server, Updated');
 });
+
+
+
+mongoose.connect('mongodb+srv://wingtsui97_db_user:o8L8J79B99g1OxNZ@ospbackenddb.3pn0whb.mongodb.net/OSP_backend?retryWrites=true&w=majority&appName=OSPBackendDB')
+.then(() => {console.log('Connected to database!');  
+})
+.catch(() => {console.log('Connection failed!');
+})
